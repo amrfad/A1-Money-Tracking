8c273c120b04c9e72ff48680a0afc657964fcd2a
Amr Fadhilah A. <amr.fadhilah.tif423@polban.ac.id>
2024-03-07 21:53:51 +0700

    Add Utilities.h and Utilities.cpp from AmrF

---
+++ Utilities.cpp
@@ -0,0 +1,12 @@
+#include "Utilities.h"
+
+/**
+ * @brief Prosedur untuk mengubah semua huruf kata menjadi uppercase
+ * 
+ * @param kata 
+ */
+void toUpperCase(char *kata) {
+    for (int i = 0; i < strlen(kata); i++) {
+        kata[i] = (kata[i] > 96 && kata[i] < 123) ? kata[i] - 32 : kata[i];
+    }
+}
---
+++ Utilities.h
@@ -0,0 +1,9 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <string.h>
+
+/*  */
+void toUpperCase(char *kata);
+
+#endif
