634cffeebd89de3489f2b7092ca9d4a98ac23d53
Amr Fadhilah A. <amr.fadhilah.tif423@polban.ac.id>
2024-03-08 13:51:21 +0700

    Update RekapHarian feature

--- RekapHarian.h
+++ RekapHarian.h
@@ -1,20 +1,22 @@
 /**
  * @file RekapHarian.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2024-03-07
+ * @author Penanggung Jawab: AMR FADHILAH ABIYYU ALIF BASYSYAR [ 231524002 ]
+ * @brief Berisi deklarasi fungsi dan prosedur yang akan digunakan pada fitur rekap harian
+ * @version 0.2
+ * @date 2024-03-08
  * 
- * @copyright Copyright (c) 2024
+ * @copyright Copyright (c) 2024 AMR FADHILAH ABIYYU ALIF BASYSYAR
  * 
  */
 
 #ifndef REKAP_HARIAN_H
 #define REKAP_HARIAN_H
 
+#include <windows.h>
 #include "User.h"
 #include "KonversiMataUang.h"
 
+/* Prototype Prosedur dan Fungsi yang Akan Digunakan */
 bool isSameDay(Tanggal tanggal1, Tanggal tanggal2);
 void rekapHarianMasuk(User user, Tanggal tanggalRekap);
 void rekapHarianKeluar(User user, Tanggal tanggalRekap);
